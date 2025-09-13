95d2acfbb0f2cee9d302ee6593046ca263dcd5a8
jeffer <chaustrejefferson@gmail.com>
2025-09-13 10:09:54 -0500

    agregada anotacion "@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)" para serializar los Page con dtos y asegurar estabilidad en los JSON

--- src/main/java/cj/esanar/ESanarRestApiApplication.java
+++ src/main/java/cj/esanar/ESanarRestApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ESanarRestApiApplication {
 
     public static void main(String[] args) {SpringApplication.run(ESanarRestApiApplication.class, args);}
